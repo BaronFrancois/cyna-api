25729c1adc67212c689dd45a8f83c00556d73d12
Kelit <theo.killian5700@gmail.com>
2026-03-09 16:15:03 +0100

    feat:flux de payement opérationel, fix:DATABASE_URL not set

--- src/v1/payement/payement.service.ts
+++ src/v1/payement/payement.service.ts
@@ -2,5 +2,13 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class PayementService {
-    
+    /* 
+        Cycle de paiement
+        Etape 1: Crée ou récupére customer
+        Etape 2: Sauvgarder une méthode de paiment si accepte
+        Etape 3: Crée un Ordre de base et le remplir
+        Etape 4: Crée et confirmer le PayementIntent
+        Etape 5: WebHook Stripe ( important sinon aucun paiement fonctionnel )
+        Etape 6: Crée l'abonnement et la facture
+    */
 }
