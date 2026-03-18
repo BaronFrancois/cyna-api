0fefa31483f800599765bc080746de3dda6e20ed
Jessica-2526 <jessica.langeland@mewo-campus.fr>
2026-03-18 09:55:59 +0100

    feat: add auth module and update app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './v1/auth/auth.module';
 import { PayementModule } from './v1/payement/payement.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PayementModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PayementModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.enableVersioning({
     type: VersioningType.URI,
